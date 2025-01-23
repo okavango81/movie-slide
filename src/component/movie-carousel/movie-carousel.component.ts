3c2e902c29f47c93d2f85f8313e812c10917a448
okavango <eguilhermeleitedev81@gmail.com>
2025-01-23 18:21:17 -0300

    done

--- src/component/movie-carousel/movie-carousel.component.ts
+++ src/component/movie-carousel/movie-carousel.component.ts
@@ -1,10 +1,9 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, AfterViewInit, ViewEncapsulation, ElementRef} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 // Importação do Swiper e seus módulos
-import Swiper, { Navigation, Pagination} from 'swiper';
+import Swiper, {Navigation, Pagination, Autoplay} from 'swiper';
 import 'swiper/swiper-bundle.min.css';
-import {Autoplay} from 'swiper';
 
 @Component({
   selector: 'app-movie-carousel',
@@ -12,27 +11,45 @@ import {Autoplay} from 'swiper';
   imports: [CommonModule],
   templateUrl: './movie-carousel.component.html',
   styleUrls: ['./movie-carousel.component.css'],
+  encapsulation: ViewEncapsulation.None, // Desativa o encapsulamento
 })
 export class MovieCarouselComponent implements AfterViewInit {
-
   movies = [
-    { title: 'Movie 1', image: 'opt_card.png' },
-    { title: 'Movie 2', image: 'op2_card.png' },
-    { title: 'Movie 3', image: 'op3_card.png' },
-    { title: 'Movie 1', image: 'opt_card.png' },
-    { title: 'Movie 2', image: 'op2_card.png' },
-    { title: 'Movie 3', image: 'op3_card.png' },
-    { title: 'Movie 1', image: 'opt_card.png' },
-    { title: 'Movie 2', image: 'op2_card.png' },
-    { title: 'Movie 3', image: 'op3_card.png' }
+    {title: 'Movie 1', image: 'edplus_img/opt/opt_card.png'},
+    {title: 'Movie 2', image:  'edplus_img/nbt/nbt_card.png'},
+    {title: 'Movie 3', image: 'edplus_img/rba/rba_card.png'},
+    {title: 'Movie 1', image: 'edplus_img/opt/op2_card.png'},
+    {title: 'Movie 2', image: 'edplus_img/bb2/bb2_card.png'},
+    {title: 'Movie 3', image: 'edplus_img/chp/chp_card.png'},
+    {title: 'Movie 3', image: 'edplus_img/chv/chv_card.png'},
+    {title: 'Movie 3', image: 'edplus_img/cra/cra_card.png'},
+    {title: 'Movie 3', image: 'edplus_img/gbt/gbt_card.png'},
+    {title: 'Movie 3', image: 'edplus_img/lts/lts_card.png'},
+    {title: 'Movie 3', image: 'edplus_img/mpd/mpd_card.png'},
+  ];
+  series = [
+    {title: 'Movie 1', image: 'edplus_img/chl/chl_card.png'},
+    {title: 'Movie 2', image:  'edplus_img/chv/chv_card.png'},
+    {title: 'Movie 3', image: 'edplus_img/lts/lts_card.png'},
+    {title: 'Movie 1', image: 'edplus_img/mpd/mpd_card.png'},
+    {title: 'Movie 2', image: 'edplus_img/toc/toc_card.png'},
+    {title: 'Movie 2', image:  'edplus_img/chv/chv_card.png'},
+    {title: 'Movie 3', image: 'edplus_img/lts/lts_card.png'},
+    {title: 'Movie 1', image: 'edplus_img/chl/chl_card.png'},
+    {title: 'Movie 2', image:  'edplus_img/chv/chv_card.png'},
+    {title: 'Movie 3', image: 'edplus_img/lts/lts_card.png'},
+    {title: 'Movie 1', image: 'edplus_img/mpd/mpd_card.png'}
   ];
 
+
+
+  constructor(private elementRef: ElementRef) {
+  }
+
   ngAfterViewInit(): void {
-    // Inicializando o Swiper com as configurações necessárias
-    new Swiper('.swiper', {
+    const swiper = new Swiper('.swiper', {
       modules: [Navigation, Pagination, Autoplay],
-      slidesPerView: 4,  // Número de slides visíveis ao mesmo tempo
-      spaceBetween: 10,  // Espaçamento entre os slides
+      spaceBetween: 30,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -42,13 +59,33 @@ export class MovieCarouselComponent implements AfterViewInit {
         el: '.swiper-pagination',
       },
       autoplay: {
-        delay: 2000, // Tempo em milissegundos entre os slides
-        disableOnInteraction: false // Continua o autoplay após interação
+        delay: 4000,
+        disableOnInteraction: false, // Não para permanentemente o autoplay
       },
-      loop: true,  // Habilitar loop de navegação
-      speed:1500
+      loop: false, // Removido loop para evitar comportamento inesperado
+      speed: 800,
+      breakpoints: {
+        350: {slidesPerView: 2},
+        480: {slidesPerView: 3},
+        520: {slidesPerView: 2},
+        640: {slidesPerView: 4},
+        768: {slidesPerView: 4},
+        1024: {slidesPerView: 6},
+        1280: {slidesPerView: 6},
+        1536: {slidesPerView: 6},
+        1600: {slidesPerView: 6},
+      },
     });
+
+    // Eventos para toque e mouse
+    const swiperElement = this.elementRef.nativeElement.querySelector('.swiper');
+
+    // Parar autoplay ao interagir
+    swiper.on('touchStart', () => swiper.autoplay.stop());
+    swiperElement.addEventListener('mouseenter', () => swiper.autoplay.stop());
+
+    // Reiniciar autoplay ao terminar interação
+    swiper.on('touchEnd', () => swiper.autoplay.start());
+    swiperElement.addEventListener('mouseleave', () => swiper.autoplay.start());
   }
 }
-
-
