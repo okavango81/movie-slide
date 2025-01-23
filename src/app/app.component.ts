dd2d8e6be0473b2d92b6aa4e417acfdad6542fd0
okavango <eguilhermeleitedev81@gmail.com>
2025-01-23 00:51:49 -0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import {MovieCarouselComponent} from '../component/movie-carousel/movie-carousel.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MovieCarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
